7daf3e49b7e2740f86b67524f650b8fda9565aba
ManuelRios18 <manuelrios18@hotmail.com>
2022-06-29 13:25:37 -0500

    Update train and test script

--- test.py
+++ test.py
@@ -1,56 +1,32 @@
-import copy
-import json
-import time
+import os
+import pickle
 import dm_env
 import numpy as np
 from dmlab2d.ui_renderer import pygame
-from ray.rllib.agents.registry import get_trainer_class
-from ray.rllib.policy.policy import PolicySpec
 from ray.tune.registry import register_env
-from examples.tutorial.harvest.configs.environment import harvest_uniandes as game
+from ray.rllib.agents.registry import get_trainer_class
 from adapters.env_creator import EnvCreator
 from adapters.ray_model_policy import RayModelPolicy
 
-
 agent_algorithm = "A3C"
 substrate_name = "commons_harvest_open"
-load_dir = "logs/"
-# checkpoint_path = "/A3C/A3C_meltingpot_87b20_00000_0_2022-06-26_19-17-29/checkpoint_000015/checkpoint-15"
-_id = 11200
-exp_name = "A3C_meltingpot_b5aba_00000_0_2022-06-27_12-58-11"
-checkpoint_path = f"/A3C/{exp_name}/checkpoint_{str(_id).zfill(6)}/checkpoint-{_id}"
+experiment_name = "A3C_meltingpot_4e667_00000_0_2022-06-29_13-14-24"
+checkpoint_id = 18
 
 
+checkpoint_path = os.path.join(agent_algorithm, experiment_name,
+                               f"checkpoint_{str(checkpoint_id).zfill(6)}",
+                               f"checkpoint-{checkpoint_id}")
 env_creator = EnvCreator()
-
-trainer_config = copy.deepcopy(get_trainer_class(agent_algorithm).get_default_config())
-trainer_config["env_config"] = game.get_config()
 register_env("meltingpot", env_creator.create_env)
 
-test_env = env_creator.create_env(trainer_config["env_config"])
-obs_space = test_env.single_player_observation_space()
-act_space = test_env.single_player_action_space()
+stored_config = pickle.load(open(os.path.join("logs", agent_algorithm, experiment_name, "params.pkl"), 'rb'))
 
-trainer_config["multiagent"] = {
-  "policies": {
-      "av": PolicySpec(
-              policy_class=None,  # use default policy
-              observation_space=obs_space,
-              action_space=act_space,
-              config={}),
-  },
-  "policy_mapping_fn": lambda agent_id, **kwargs: "av"
-}
+trainer = get_trainer_class(agent_algorithm)(env="meltingpot", config=stored_config)
+trainer.restore(os.path.join("logs", checkpoint_path))
 
-with open(load_dir + '/config.json', 'r') as f:
-    trainer_config.update(json.load(f))
-
-trainer = get_trainer_class(agent_algorithm)(
-  env="meltingpot", config=trainer_config)
-
-trainer.restore(load_dir + checkpoint_path)
-env = test_env._env
-bots = [RayModelPolicy(trainer, "av")] * trainer_config["env_config"]["num_players"]
+env = env_creator.create_env(stored_config["env_config"]).get_dmlab2d_env()
+bots = [RayModelPolicy(trainer, "av")] * stored_config["env_config"]["num_players"]
 
 timestep = env.reset()
 states = [bot.initial_state() for bot in bots]
@@ -61,14 +37,7 @@ pygame.init()
 pygame.display.set_caption('DM Lab2d')
 shape = env.observation_spec()[0]['WORLD.RGB'].shape
 game_display = pygame.display.set_mode((int(shape[1] * scale), int(shape[0] * scale)))
-reward_hist = {bot_num:0 for bot_num in range(len(bots))}
-reward_hist["total"] = 0
-zap_hist = list()
-zap_mem = 0
-red_hist = list()
-blue_hist = list()
-green_hist = list()
-total_reward_hist = list()
+
 for sim_step in range(1000):
     obs = timestep.observation[0]['WORLD.RGB']
     obs = np.transpose(obs, (1, 0, 2))
@@ -80,22 +49,13 @@ for sim_step in range(1000):
     pygame.display.update()
 
     for i, bot in enumerate(bots):
-      timestep_bot = dm_env.TimeStep(
+        timestep_bot = dm_env.TimeStep(
           step_type=timestep.step_type,
           reward=timestep.reward[i],
           discount=timestep.discount,
           observation=timestep.observation[i])
 
-      actions[i], states[i] = bot.step(timestep_bot, states[i])
-
+        actions[i], states[i] = bot.step(timestep_bot, states[i])
     timestep = env.step(actions)
-    acum = 0
-    for i, r in enumerate(timestep.reward):
-        reward_hist[i] += float(r)
-        acum += reward_hist[i]
-    reward_hist["total"] = acum
     if sim_step%100 == 0:
         print(f"step ", sim_step)
-    #time.sleep(0.05)
-
-
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import ray
 import copy
-import json
 import platform
 from ray import tune
 from adapters.env_creator import EnvCreator
@@ -11,11 +10,12 @@ from examples.tutorial.harvest.configs.environment import harvest_uniandes as ga
 
 
 agent_algorithm = "A3C"
+n_steps = 16000000
+checkpoint_freq = 1
+keep_checkpoints_num = 1
 num_cpus = 1
-save_dir = "logs"
 
 n_gpus = 0 if platform.system() == "Darwin" else 1
-
 trainer_config = copy.deepcopy(get_trainer_class(agent_algorithm).get_default_config())
 trainer_config["env_config"] = game.get_config()
 
@@ -35,50 +35,45 @@ model = {"conv_filters": [[16, [8, 8], 8], [128, [11, 11], 1]],
          "lstm_cell_size": 256
          }
 
-config = {"env": "meltingpot",
-          "callbacks": MetricsCallback,
-          "env_config": trainer_config["env_config"],
-          "num_gpus": n_gpus,
-          "num_workers": 1,
-          "horizon": trainer_config["env_config"].lab2d_settings["maxEpisodeLengthFrames"],
-          "batch_mode": "complete_episodes",
-          "rollout_fragment_length": 1,
-          "train_batch_size": trainer_config["env_config"].lab2d_settings["maxEpisodeLengthFrames"],
-          #"sgd_minibatch_size": 128,
-          "no_done_at_end": False,
-          "model": model,
-          "evaluation_interval": 50,
-          "evaluation_duration": 1,
-          "evaluation_duration_unit": "episodes",
-          #"evaluation_num_workers": 1,
-          "multiagent":
-              {
-                "policies": {
-                "av": PolicySpec(
-                policy_class=None,
-                observation_space=obs_space,
-                action_space=act_space,
-                config={}),
-                },
-                "policy_mapping_fn": lambda agent_id, **kwargs: "av"
-                }
-              }
-
-stop = {"timesteps_total": 16000000}
+config = {
+    "env": "meltingpot",
+    "callbacks": MetricsCallback,
+    "env_config": trainer_config["env_config"],
+    "num_gpus": n_gpus,
+    "num_workers": 1,
+    "horizon": trainer_config["env_config"].lab2d_settings["maxEpisodeLengthFrames"],
+    "batch_mode": "complete_episodes",
+    "rollout_fragment_length": 1,
+    "train_batch_size": trainer_config["env_config"].lab2d_settings["maxEpisodeLengthFrames"],
+    "no_done_at_end": False,
+    "model": model,
+    "evaluation_interval": 50,
+    "evaluation_duration": 1,
+    "evaluation_duration_unit": "episodes",
+    "multiagent":
+    {
+        "policies": {
+        "av": PolicySpec(
+        policy_class=None,
+        observation_space=obs_space,
+        action_space=act_space,
+        config={}),
+        },
+        "policy_mapping_fn": lambda agent_id, episode, worker, **kwargs: "av",
+    }
+}
 
 
 tune.register_env("meltingpot", env_creator.create_env)
 ray.init(num_cpus=num_cpus + 1)
 
-with open(save_dir + "/config.json", "w") as f:
-    target_keys = ["num_gpus", "num_workers", "rollout_fragment_length", "train_batch_size", "horizon",
-                   "no_done_at_end", "model"]
-    config_to_store = dict()
-    for tk in target_keys:
-        config_to_store[tk] = config[tk]
-    json.dump(config_to_store, f)
-
-results = tune.run(agent_algorithm, config=config, stop=stop, local_dir=save_dir,
-         checkpoint_freq=200,
-         keep_checkpoints_num=None, # 1 to debug
-         checkpoint_score_attr='training_iteration')
+results = tune.run(agent_algorithm,
+                   stop={"timesteps_total": n_steps},
+                   config=config,
+                   local_dir="logs",
+                   checkpoint_freq=checkpoint_freq,
+                   keep_checkpoints_num=keep_checkpoints_num,
+                   checkpoint_score_attr='training_iteration',
+                   metric="episode_reward_mean",
+                   mode="max",
+                   checkpoint_at_end=True)
