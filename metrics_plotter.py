945b8f8dd181c8917ab223138d49274da68612b6
ManuelRios18 <manuelrios18@hotmail.com>
2022-09-05 15:14:46 -0500

    Add dreamer log data loader

--- metrics_plotter.py
+++ metrics_plotter.py
@@ -1,3 +1,27 @@
+import utils
+import matplotlib.pyplot as plt
+
+
+path_dreamer = "logs/dreamer_multiagent_test/events.out.tfevents.1661978438.PCcasa.523154.0.v2"
+path_ppo = "logs/dreamer_multiagent_test/progress_ppo.csv"
+
+dreamer_metrics = utils.parse_dreamer_logs(path_dreamer, "scalars/train_return")
+ppo_metrics = utils.parse_metrics(path_ppo)
+
+dreamer_smoothed = utils.smooth(dreamer_metrics["metric"], 0.95)
+
+plt.figure()
+plt.plot(ppo_metrics["episodes"][:4000], ppo_metrics["efficiency"][:4000], label="PPO")
+plt.plot(dreamer_metrics["episodes"], dreamer_smoothed, label="Dreamer-MA")
+plt.axhline(y=20.87, color='black', linestyle='--', label="Random Agents")
+plt.legend()
+plt.grid()
+plt.xlabel("Episode")
+plt.ylabel("Efficiency")
+plt.show()
+
+
+"""
 import os
 import utils
 import shutil
@@ -38,3 +62,4 @@ plt.ylabel("Equality")
 plt.title("Equality vs Episodes")
 plt.grid()
 plt.savefig(os.path.join(target_dir, "equality.pdf"))
+"""
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import tensorflow as tf
 
 
 def load_config():
@@ -18,3 +19,26 @@ def parse_metrics(progress_file_path):
                "reward": progress.episode_reward_mean.to_numpy()}
     return metrics
 
+
+def parse_dreamer_logs(path, metric_name):
+    counter = 0
+    hist = list()
+    episodes = list()
+    for e in tf.compat.v1.train.summary_iterator(path):
+        for v in e.summary.value:
+            if v.tag == metric_name:
+                hist.append(float(tf.make_ndarray(v.tensor)))
+                episodes.append(e.step/1000)
+                counter += 1
+    return {"episodes": episodes, "metric": hist}
+
+
+def smooth(scalars, weight):  # Weight between 0 and 1
+    last = scalars[0]  # First value in the plot (first timestep)
+    smoothed = list()
+    for point in scalars:
+        smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
+        smoothed.append(smoothed_val)                        # Save it
+        last = smoothed_val                                  # Anchor the last smoothed value
+
+    return smoothed
